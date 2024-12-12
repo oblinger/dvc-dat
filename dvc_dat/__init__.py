43496b2150b4ac617d38d04da2655424f028c2bc
Dan Oblinger <code@oblinger.us>
2024-12-11 22:09:02 -0800

    Added basic Metric support, and updated to_execl with many additional options

--- dvc_dat/__init__.py
+++ dvc_dat/__init__.py
@@ -1,21 +1,28 @@
 __version__ = "1.00.05"
 DAT_VERSION = f"{__version__} (2024-06-20)"
 
+import __main__ as main
 from .dat import dat_manager, Dats, _DAT_MOUNT_COMMANDS
 from .do_fn import DoManager, do_argv
-
-dats = dat_manager
-do = dats.do = DoManager()  # not available during load of do_fn
-
 from .dat import Dat, DatContainer
 from . import dat_tools
 
-cmds = dats.config.get(_DAT_MOUNT_COMMANDS, [])
-do.mount_all(cmds, relative_to=dats.folder)
-do.mount(module=dat_tools, at="dat_tools")
-do.mount(module=dat_tools, at="dt")
-do.mount(value=dat_tools.cmd_list, at="dt.list")
-do.mount(value=dat_tools.cmd_list, at="dat_tools.list")
+
+if not hasattr(main, "NO_DAT_DVC_INIT"):
+    not here
+    dats = dat_manager
+    do = dats.do = DoManager()  # not available during load of do_fn
+
+    from .dat import Dat, DatContainer
+    from . import dat_tools
+
+    cmds = dats.config.get(_DAT_MOUNT_COMMANDS, [])
+    do.mount_all(cmds, relative_to=dats.folder)
+    do.mount(module=dat_tools, at="dat_tools")
+    do.mount(module=dat_tools, at="dt")
+    do.mount(value=dat_tools.cmd_list, at="dt.list")
+    do.mount(value=dat_tools.cmd_list, at="dat_tools.list")
+
 
 __all__ = [
     "DAT_VERSION",
